5c5aca5d3f400181c6b988eb52aacf210d73a9ee
Brandon <brandon101morgan@outlook.com>
2025-07-23 09:36:41 -0500

    APU

---
+++ SmartPlantApp-v1KIOSK/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace APU
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.APUview = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.APUview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button1.Location = new System.Drawing.Point(15, 15);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "How do I log in?";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // APUview
+            // 
+            this.APUview.AllowExternalDrop = true;
+            this.APUview.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.APUview.CreationProperties = null;
+            this.APUview.DefaultBackgroundColor = System.Drawing.Color.Gainsboro;
+            this.APUview.Location = new System.Drawing.Point(15, 72);
+            this.APUview.Name = "APUview";
+            this.APUview.Size = new System.Drawing.Size(890, 649);
+            this.APUview.TabIndex = 2;
+            this.APUview.ZoomFactor = 1D;
+            this.APUview.Click += new System.EventHandler(this.APUview_Click);
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(166, 15);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(176, 35);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Cant Connect?";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(368, 20);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(80, 20);
+            this.linkLabel1.TabIndex = 5;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "MaintainX";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(500, 20);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(88, 20);
+            this.linkLabel2.TabIndex = 6;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "SmartPlant\r\n";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(922, 702);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.APUview);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.Name = "Form1";
+            this.Text = "SmartPlant";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.APUview)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private Microsoft.Web.WebView2.WinForms.WebView2 APUview;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+    }
+}
+
---
+++ SmartPlantApp-v1KIOSK/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Web.WebView2.Core;
+
+namespace APU
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += Form1_Load;
+            this.TopMost = true;
+            this.FormBorderStyle = FormBorderStyle.None;
+            //this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.WindowState = FormWindowState.Maximized;
+
+        }
+       
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            await APUview.EnsureCoreWebView2Async(null);
+            // APUview.CoreWebView2.Navigate("//office.com");
+            // Smartplant Liv2 192.168.8.11 
+            // SMartplant Liv1 192.168.5.11
+            APUview.CoreWebView2.Navigate("https://lv2-smartplant.hutchinsonna.com:8443/LivingstonSmartPlantLIVE/celldashboard/celldashboardmenu.jsp?");
+            //APUview.Dock = DockStyle.Fill;
+            this.Controls.Add(APUview);
+            //APUview.CoreWebView2.NewWindowRequested += CoreWebView2_NewWindowRequested;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("example: ID cell16 passwd cl16");
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ensure you are connected to the production wifi (Hutchinson) or HNA_Network wifi. If this device has disconnected and needs network password contact local IT");
+
+        }
+        private void APUview_Click(object sender, EventArgs e)
+        {
+            if (APUview.CoreWebView2 != null)
+            {
+                // APUview.CoreWebView2.Navigate("//office.com");
+                APUview.CoreWebView2.Navigate("https://lv2-smartplant.hutchinsonna.com:8443/LivingstonSmartPlantLIVE/celldashboard/celldashboardmenu.jsp?");
+            }
+            else
+            {
+                MessageBox.Show("WebView2 is not initialized yet!");
+            }
+            APUview.Focus();
+        }
+        
+        /*
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = FormBorderStyle.Fixed3D;
+        }
+        */
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            // Specify the URL you want to navigate to
+            string url = "https://app.getmaintainx.com/auth/login?~channel=Direct%20Traffic&~feature=organic&~campaign=%2Flogin&~last_page_seen=https%3A%2F%2Fwww.getmaintainx.com%2Flogin&gclid=EAIaIQobChMI7ZubrYKAjAMVcS7UAR0YWBlIEAAYASAAEgJskfD_BwE&_branch_match_id=1377659230372389608&utm_source=Direct%20Traffic&utm_campaign=%2Flogin&utm_medium=organic&_branch_referrer=H4sIAAAAAAAAA2XNTUvDQBSF4V%2BTLJtSoQVhkNs2QpQutGqJm3BnvPNRJzPhZsK4ym833QnCOZtn89qUhvG%2BqiSvDKUeXUjLf1Yq9tWFAtitVtdJPhjl3ZeoocHmJcqDPTW7z0ly%2BwxXOH2o8%2B4dXtftZe%2BbGqCFM0BtnsZvfez2uS5nj2PqBjTUjURB2Fu1uINi87gs5%2FwvvrCPxoVyVtgP6EwQf8hiCOTF0TGpVGzWb4xaO1XOmjBNTCKywXADJk3MLphOcswjsThYjj39AmyRneD4AAAA";
+
+            // Use Process.Start to open the URL in the default web browser
+            //System.Diagnostics.Process.Start(url);
+            APUview.CoreWebView2.Navigate("https://app.getmaintainx.com/auth/login?~channel=Direct%20Traffic&~feature=organic&~campaign=%2Flogin&~last_page_seen=https%3A%2F%2Fwww.getmaintainx.com%2Flogin&gclid=EAIaIQobChMI7ZubrYKAjAMVcS7UAR0YWBlIEAAYASAAEgJskfD_BwE&_branch_match_id=1377659230372389608&utm_source=Direct%20Traffic&utm_campaign=%2Flogin&utm_medium=organic&_branch_referrer=H4sIAAAAAAAAA2XNTUvDQBSF4V%2BTLJtSoQVhkNs2QpQutGqJm3BnvPNRJzPhZsK4ym833QnCOZtn89qUhvG%2BqiSvDKUeXUjLf1Yq9tWFAtitVtdJPhjl3ZeoocHmJcqDPTW7z0ly%2BwxXOH2o8%2B4dXtftZe%2BbGqCFM0BtnsZvfez2uS5nj2PqBjTUjURB2Fu1uINi87gs5%2FwvvrCPxoVyVtgP6EwQf8hiCOTF0TGpVGzWb4xaO1XOmjBNTCKywXADJk3MLphOcswjsThYjj39AmyRneD4AAAA");
+        }
+        private void linkLabel2_LinkClicked_1(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            APUview.CoreWebView2.Navigate("https://lv2-smartplant.hutchinsonna.com:8443/LivingstonSmartPlantLIVE/celldashboard/celldashboardmenu.jsp?");
+        }
+        //stop those pesky people from exiting via Alt f4 # Forced Kiosk Mode 
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            switch (e.CloseReason)
+            {
+                case CloseReason.UserClosing:
+                    e.Cancel = true;
+                    break;
+            }
+
+            base.OnFormClosing(e);
+        }
+    }
+}
